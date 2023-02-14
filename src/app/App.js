514eacfc453cbb20da0a94a64e77257674414be0
BetaKrankri <edominguezj11@gmail.com>
2023-02-13 18:20:59 -0600

    first layout

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import GameBoard from '../features/gameBoard/GameBoard.js';
+import GameBoard from '../features/Board/Board.js';
 
 function App() {
   return (
     <div className="App">
-      <h1 id='game-name'><span>3rd</span> Transformation</h1>
+      <h1 id='game-name'><span>3</span>Txc</h1>
       <GameBoard />
     </div>
   );
---
+++ src/features/Board/Board.js
@@ -0,0 +1,12 @@
+import './Board.css';
+
+function GameBoard(props) {
+    return ( 
+        <div  className='GameBoard'>
+            
+
+        </div>
+     );
+}
+
+export default GameBoard;
